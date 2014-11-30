c935d6453d9896e64a1b3e7fb10dfa53f670582a
sara.ed <s1442177@sms.ed.ac.uk>
2014-11-30 12:53:53 +0000

    adding drone

--- index1.js
+++ index1.js
@@ -1,6 +1,44 @@
 var http = require('http');
 var express = require('express');
-var app = express();
+var app = express();var Parse = require('parse').Parse;
+
+
+var jsonObject = JSON.parse(data);
+var speedx = jsonObject.x;
+var speedy = jsonObject.y;
+var speedz = jsonObject.z;
+
+var update = function(speedx, speedy, speedz){
+	if (speedz > 0){
+		client.front(speedz);
+		console.log('front', speedz);
+	}
+	else{
+		client.back(-1*speedz);
+		console.log('back', speedz);
+	};
+	if (speedy > 0){
+		client.up(speedy);
+	}
+	else{
+		client.down(-1*speedy);
+	}
+	if (speedx > 0){
+		client.right(speedx);
+	}
+	else{
+		client.left(-1*speedx);
+	}
+	fs.writeFile(speedx, speedy, speedz, function(err) {
+    if(err) {
+        console.log(err);
+    } 
+	else {
+        console.log("The file was saved!");
+    };
+}); 
+};
+
 
 var speedx = 0.0
 var speedy = 0.0
@@ -70,6 +108,7 @@ var handle = function(req, res){
 	else if(req.url === '/tem'){
 		control.ref({fly: true, emergency: false});
 	}
+	update(speedx, speedy, speedz);
 //	res.send({"x": speedx, "y": speedy, "z": speedz})
 	res.writeHead(200, {"Content-Type": "text/json"});
 	res.write(JSON.stringify({"x": speedx, "y": speedy, "z": speedz}));
